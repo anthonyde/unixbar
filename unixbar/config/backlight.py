e720288b21bccdc5f4424b0908e3836718df1b0a
Anthony DeRossi <ajderossi@gmail.com>
2018-03-01 23:36:30 -0800

    Switch to logarithmic mapping for brightness
    The system brightness control is no longer linear.

--- unixbar/config/backlight.py
+++ unixbar/config/backlight.py
@@ -16,6 +16,11 @@ BL_SYM = "\ue8af" # i3fonticon
 
 BL_N_MAX = 16
 
+def bl_n(bness, bness_max):
+  """Map backlight brightness [0, bness_max] onto [0, n_max]."""
+  k = util.log_scale(BL_N_MAX, bness_max)
+  return int(util.log_transform(k, bness) + 1 / 2)
+
 @data.transformer(keys=["bl_bness", "bl_bness_max", "bl_power"])
 @data.on_val()
 def transform_to_int(v):
@@ -29,7 +34,7 @@ def view_bl(osd_out, bl_bness=0, bl_bness_max=0, bl_power=0, **ds):
   if not view_bl._first and view_bl._last_power == bl_power:
     print("%{{c}}{sym}\n%{{c}}%{{T2}}{bness_bar}%{{T-}}".format(
         sym=BL_SYM,
-        bness_bar=util.prog_bar(bl_bness * BL_N_MAX // bl_bness_max, BL_N_MAX)
+        bness_bar=util.prog_bar(bl_n(bl_bness, bl_bness_max), BL_N_MAX)
         ),
       file=osd_out
       )
