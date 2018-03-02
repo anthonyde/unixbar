500ff4f3c4dda8ebd54b7b0cb3d764b74ab69983
Anthony DeRossi <ajderossi@gmail.com>
2018-03-01 23:17:41 -0800

    Add exponential/logarithmic curve utilities
    These functions simplify exponential/logarithmic <-> linear mapping,
    e.g. for audio levels.

--- unixbar/config/util.py
+++ unixbar/config/util.py
@@ -2,11 +2,25 @@
 Miscellaneous configuration things that don't fit anywhere else
 """
 
+import math
+
 from . import chars
 
 from ..util import *
 
 __all__ = []
+
+def log_scale(x, y):
+  """Calculate a value k such that x * log(10) = k * log(y + 1)."""
+  return x * math.log(10) / math.log(y + 1)
+
+def exp_transform(k, x):
+  """Map a value x onto an exponential curve defined by k."""
+  return 10 ** (x / k) - 1
+
+def log_transform(k, y):
+  """Map a value y onto a logarithmic curve defined by k."""
+  return k * math.log(y + 1) / math.log(10)
 
 def nonempty_keys(d):
   """Get a set of non-empty keys in a dict."""
